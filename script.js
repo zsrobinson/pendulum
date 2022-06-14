c33a6a217cf5bfb5ec1df786c7cf11a4193872e2
Zachary Robinson <zachary_robinson@icloud.com>
2022-06-13 21:33:45 -0400

    UI looks a lot better

--- script.js
+++ script.js
@@ -8,30 +8,31 @@ let color = {
 	border: "rgb(51, 51, 51)"
 }
 
-let len1 = 150;
-let len2 = 150;
-let mass1 = 20;
-let mass2 = 20;
-let angle1 = Math.PI/4;
-let angle2 = Math.PI/2;
+let len1, len2, mass1, mass2, angle1, angle2, vel1, vel2, g, damp, xoffset, yoffset, px2, py2, trail, firstFrame;
 
-let vel1 = 0;
-let vel2 = 0;
-
-let g = 1
-
-let xoffset, yoffset;
-let px2, py2; // previous x2 and y2 for the trail
-
-let trail;
+function setVars() {
+	len1 = 150;
+	len2 = 150;
+	mass1 = 20;
+	mass2 = 20;
+	angle1 = 5*Math.PI/8;
+	angle2 = 6*Math.PI/8;
+	vel1 = 0;
+	vel2 = 0;
+	g = 1
+	damp = 0.001;
+	firstFrame = true;
+	xoffset = width/2;
+	yoffset = height/4;
+}
 
 function setup() {
 	var div = document.getElementById("sim");
 	var canvas = createCanvas(div.offsetWidth, div.offsetHeight);
 	canvas.parent('sim');
 
-	xoffset = width/2;
-	yoffset = height/4;
+	setVars();
+	noLoop();
 
 	trail = createGraphics(div.offsetWidth, div.offsetHeight);
 	trail.background(color.theme);
@@ -42,8 +43,8 @@ function draw() {
 	
 	image(trail, 0, 0)
 
-	acc1 = (-g*(2*mass1+mass2)*Math.sin(angle1) - mass2*g*Math.sin(angle1-2*angle2) - 2*Math.sin(angle1-angle2)*mass2*(vel2*vel2*len2+vel1*vel1*len1*Math.cos(angle1-angle2))) / (len1*(2*mass1+mass2-mass2*Math.cos(2*angle1-2*angle2)))
-	acc2 = (2*Math.sin(angle1-angle2)*(vel1*vel1*len1*(mass1+mass2)+g*(mass1+mass2)*Math.cos(angle1)+vel2*vel2*len2*mass2*Math.cos(angle1-angle2)))/(len2*(2*mass1+mass2-mass2*Math.cos(2*angle1-2*angle2)))
+	let acc1 = (-g*(2*mass1+mass2)*Math.sin(angle1) - mass2*g*Math.sin(angle1-2*angle2) - 2*Math.sin(angle1-angle2)*mass2*(vel2*vel2*len2+vel1*vel1*len1*Math.cos(angle1-angle2))) / (len1*(2*mass1+mass2-mass2*Math.cos(2*angle1-2*angle2)))
+	let acc2 = (2*Math.sin(angle1-angle2)*(vel1*vel1*len1*(mass1+mass2)+g*(mass1+mass2)*Math.cos(angle1)+vel2*vel2*len2*mass2*Math.cos(angle1-angle2)))/(len2*(2*mass1+mass2-mass2*Math.cos(2*angle1-2*angle2)))
 
 	stroke(color.primary);
 	strokeWeight(4);
@@ -63,29 +64,41 @@ function draw() {
 	ellipse(x1,y1,mass1,mass1)
 	ellipse(x2,y2,mass2,mass2)
 
-	
-
-
-
 	vel1 += acc1;
 	vel2 += acc2;
 	angle1 += vel1;
 	angle2 += vel2;
 
-	vel1 *= 0.999;
-	vel2 *= 0.999;
+	vel1 *= 1 - damp;
+	vel2 *= 1 - damp;
 
 	trail.stroke(color.secondary)
 	trail.strokeWeight(4);
 	trail.fill(100);
-	trail.line(px2, py2, x2, y2)
+	if (!firstFrame) {
+		trail.line(px2, py2, x2, y2)
+	} else {
+		firstFrame = false;
+	}
 	trail.background(color.themetransparent)
 
 	px2 = x2;
 	py2 = y2;
-	
 }
 
 window.onresize = () => {
 	setup();
 }
+
+document.getElementById("restart-button").addEventListener("click", () => {
+	loop();
+	setup();
+});
+
+document.getElementById("pause-button").addEventListener("click", () => {
+	noLoop();
+});
+
+document.getElementById("play-button").addEventListener("click", () => {
+	loop();
+});
